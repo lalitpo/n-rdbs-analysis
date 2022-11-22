4dda28c61540c3d81fa45ae4963fd652e6f05118
lalit <lalit.dar4@gmail.com>
2022-11-22 16:10:53 +0100

    json creation

--- com/dataprep/PreProcessAndLoad.py
+++ com/dataprep/PreProcessAndLoad.py
@@ -25,18 +25,25 @@ proc_attribute = configs.get("conf_proc_tags").data
 inproc_attribute = configs.get("conf_article_tags").data
 
 
+def add_elem(ent1, item):
+    if ent1.tag in item.keys() and ent1.text is not None:
+        item[ent1.tag] = item.get(ent1.tag)+","+ent1.text
+    else:
+        item[ent1.tag] = ent1.text
+
+
 def create_data(elem):
     item = dict()
     item['key'] = elem.attrib.get('key')
     for ent1 in elem.getchildren():
         if elem.tag == article and ent1.tag in article_attribute:
-            item[ent1.tag] = ent1.text
+            add_elem(ent1, item)
             article_list.append(item)
         if elem.tag == proceedings and ent1.tag in proc_attribute:
-            item[ent1.tag] = ent1.text
+            add_elem(ent1, item)
             proc_list.append(item)
         if elem.tag == inproceedings and ent1.tag in inproc_attribute:
-            item[ent1.tag] = ent1.text
+            add_elem(ent1, item)
             inproc_list.append(item)
 
 
@@ -55,12 +62,15 @@ article_df = (pd.DataFrame.from_dict(dblp[article]).dropna()).drop_duplicates()
 inproc_df = (pd.DataFrame.from_dict(dblp[inproceedings])).drop_duplicates()
 proc_df = (pd.DataFrame.from_dict(dblp[proceedings])).drop_duplicates()
 
-presto_db_conn = prestodb.dbapi.connect(host=configs.get("PRESTO_DB_HOST").data,
+article_json = article_df.to_json(r'article.json', orient='records')
+inproc_json = inproc_df.to_json(r'inproceedings.json', orient='records')
+proc_json = proc_df.to_json(r'proceedings.json', orient='records')
+
+"""presto_db_conn = prestodb.dbapi.connect(host=configs.get("PRESTO_DB_HOST").data,
                                         port=8080,
-                                        http_scheme='https',
                                         catalog='dblp-catalog',
                                         schema=configs.get("PRESTO_DB_SCHEMA").data,
                                         auth=prestodb.auth.BasicAuthentication(configs.get("PRESTO_DB_User").data,
                                                                                configs.get("PRESTO_DB_PWD").data),
                                         verify=False)
-cur = presto_db_conn.cursor()
+cur = presto_db_conn.cursor()"""
