8985c2356bbcb44a65fe5a79b776a51f82059a6b
Minigrim0 <grimauflorent@gmail.com>
2022-12-17 17:41:08 +0100

    [Fix] Multi loading in redis

--- com/dataprep/LoadRedis.py
+++ com/dataprep/LoadRedis.py
@@ -1,12 +1,13 @@
 import redis
 import json
+import time
 
 def update_progress(subject, progress):
     print(
         "\r{0} [{1}] {2}%".format(
             subject,
             ("#"*int(progress//2) + " "*int((102-progress)//2)),
-            str(round(progress, 2)).rjust(6)
+            str(int(progress)).rjust(3)
         ),
         end=''
     )
@@ -21,28 +22,77 @@ def load_json(json_file: str):
 def load_articles(r: redis.StrictRedis, json_file: str):
     data = load_json(json_file)
     length = len(data)
+    start = time.process_time()
+    r.execute_command("MULTI")
     for i, article in enumerate(data):
         update_progress("Loading articles", (i/length)*100)
-        r.execute_command("HSET", f"article:{i}", "key", article["key"], "title", article["title"], "year", article["year"], "journal", article["journal"], "number", article["number"], "volume", article["volume"])
-    print()
+        r.execute_command(
+            "HSET", f"article:{i}",
+            "key", article["key"],
+            "title", article["title"],
+            "year", article["year"],
+            "journal", article["journal"],
+            "number", article["number"],
+            "volume", article["volume"]
+        )
+        # Execute the command every 1000 articles
+        if i % 1000 == 0:
+            r.execute_command("EXEC")
+            r.execute_command("MULTI")
+    r.execute_command("EXEC")
+    end = time.process_time()
+    print(f"\nTime: {round(end-start, 2)}s")
 
 
 def load_inproceedings(r: redis.StrictRedis, json_file: str):
     data = load_json(json_file)
     length = len(data)
+    start = time.process_time()
+    r.execute_command("MULTI")
     for i, inproceedings in enumerate(data):
         update_progress("Loading inproceedings", (i/length)*100)
-        r.execute_command("HSET", f"inproceedings:{i}", "key", inproceedings["key"], "author", inproceedings["author"], "title", inproceedings["title"], "year", inproceedings["year"], "pages", inproceedings["pages"])
-    print()
+        r.execute_command(
+            "HSET", f"inproceedings:{i}",
+            "key", inproceedings["key"] if inproceedings["key"] is not None else "",
+            "author", inproceedings["author"] if inproceedings["author"] is not None else "",
+            "title", inproceedings["title"] if inproceedings["title"] is not None else "",
+            "year", inproceedings["year"] if inproceedings["year"] is not None else "",
+            "pages", inproceedings["pages"] if inproceedings["pages"] is not None else ""
+        )
+        # Execute the command every 1000 inproceedings
+        if i % 1000 == 0:
+            r.execute_command("EXEC")
+            r.execute_command("MULTI")
+
+    r.execute_command("EXEC")
+    end = time.process_time()
+    print(f"\nTime: {round(end-start, 2)}s")
 
 
 def load_proceedings(r: redis.StrictRedis, json_file: str):
     data = load_json(json_file)
     length = len(data)
+    start = time.process_time()
+    r.execute_command("MULTI")
     for i, proceedings in enumerate(data):
         update_progress("Loading proceedings", (i/length)*100)
-        r.execute_command("HSET", f"proceedings:{i}", "key", proceedings["key"], "editor", proceedings["editor"], "title", proceedings["title"], "publisher", proceedings["publisher"], "year", proceedings["year"], "volume", proceedings["volume"])
-    print()
+        r.execute_command(
+            "HSET", f"proceedings:{i}",
+            "key", proceedings["key"] if proceedings["key"] is not None else "",
+            "editor", proceedings["editor"] if proceedings["editor"] is not None else "",
+            "title", proceedings["title"] if proceedings["title"] is not None else "",
+            "publisher", proceedings["publisher"] if proceedings["publisher"] is not None else "",
+            "year", proceedings["year"] if proceedings["year"] is not None else "",
+            "volume", proceedings["volume"] if proceedings["volume"] is not None else ""
+        )
+        # Execute the command every 1000 proceedings
+        if i % 1000 == 0:
+            r.execute_command("EXEC")
+            r.execute_command("MULTI")
+
+    r.execute_command("EXEC")
+    end = time.process_time()
+    print(f"\nTime: {round(end-start, 2)}s")
 
 
 if __name__ == '__main__':
@@ -51,11 +101,15 @@ if __name__ == '__main__':
 
     # DROP INDEX
     try:
+        print("Dropping indexes...")
+        start = time.process_time()
         r.execute_command("FT.DROPINDEX articles DD")
         r.execute_command("FT.DROPINDEX inproceedings DD")
         r.execute_command("FT.DROPINDEX proceedings DD")
+        end = time.process_time()
+        print(f"Indexes dropped in {round(end-start, 2)}s")
     except:
-        pass
+        print("Indexes not found")
 
     # Create the index
     article = "FT.CREATE articles ON HASH PREFIX 1 article: SCHEMA key TEXT title TEXT WEIGHT 5.0 year NUMERIC journal TEXT number NUMERIC volume NUMERIC"
